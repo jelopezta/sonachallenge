2fbe1cc1a59a1df56f404a6b18d47a123d0c23e9
jelopezta <jelopezta@gmail.com>
2020-01-27 22:46:54 -0500

    Prevent call to API with empty or invalid value in javascript

--- src/main/webapp/js/functions.js
+++ src/main/webapp/js/functions.js
@@ -1,19 +1,25 @@
 function convertNumber() {
 	var numberInput = document.getElementById('numberToTransform');
 	let basePath = document.location.origin + '/sonatypechallenge/rest/englishconverter/' + numberInput.value;
-	fetch(basePath)
-	  .then((response) => {
-	    return response.text();
-	  })
-	  .then((text) => {
-		 var resultLabel =document.getElementById('responseLabel')
-		 resultLabel.innerText = text;
-	  });
+	if (numberInput.value !== "") {
+		fetch(basePath)
+			.then((response) => {
+				return response.text();
+			})
+			.then((text) => {
+				var resultLabel = document.getElementById('responseLabel')
+				resultLabel.innerText = text;
+			});
+	} else {
+		var resultLabel = document.getElementById('responseLabel')
+		resultLabel.innerText = 'Number is empty or has an invalid value. Please check your input and try again';
+	}
+
 }
 
 function clearResult() {
-		 var resultLabel =document.getElementById('responseLabel')
-		 resultLabel.innerText = '';
-		 var numberInput = document.getElementById('numberToTransform');
-		 numberInput.value = '';
+	var resultLabel = document.getElementById('responseLabel')
+	resultLabel.innerText = '';
+	var numberInput = document.getElementById('numberToTransform');
+	numberInput.value = '';
 }
